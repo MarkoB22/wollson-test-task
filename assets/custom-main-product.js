408d282d2a59dab9bd72eeeb31b8e7cfc086c7a1
MarkoB <bmarko15@yahoo.com>
2025-02-06 11:32:14 +0100

    Reviews, product content, start of variants selectors

--- assets/custom-main-product.js
+++ assets/custom-main-product.js
@@ -1,28 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
     const thumbSwiper = new Swiper(".custom_main_product-thumbnails_wrapper", {
-      spaceBetween: 6.5,
-      slidesPerView: "auto",
-      freeMode: true,
-      watchSlidesProgress: true,
-      watchSlidesVisibility: true,
+        spaceBetween: 6.5,
+        slidesPerView: 6,
+        freeMode: true,
+        observer: true,
+        observeParents: true,
+        watchSlidesProgress: true,
+        watchSlidesVisibility: true,
     });
-  
+
     const mainSwiper = new Swiper(".custom_main_product-swiper", {
-      loop: true,
-      spaceBetween: 10,
-      pagination: { el: ".swiper-pagination", clickable: true },
-      navigation: {
+        loop: true,
+        spaceBetween: 10,
+        observer: true,
+        observeParents: true,
+        watchOverflow: true,
+        pagination: { el: ".swiper-pagination", clickable: true },
+        navigation: {
         nextEl: ".custom-swiper-button-next",
         prevEl: ".custom-swiper-button-prev",
-      },
-      thumbs: { swiper: thumbSwiper },
+        },
+        thumbs: { swiper: thumbSwiper },
     });
-  
+
     document.querySelectorAll(".custom_main_product-thumbnail").forEach((thumb, index) => {
-      thumb.addEventListener("click", () => {
+        thumb.addEventListener("click", () => {
         mainSwiper.slideToLoop(index);
-      });
+        });
     });
+});
   
-  });
+
+  
+  
+  
+  
+  
+  
+  
   
